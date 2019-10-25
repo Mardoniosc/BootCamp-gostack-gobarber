9915b4cd6530ef0c9fcf266b474a0e488f1c2274
Mardoniosc <Mardonio@live.com>
2019-10-24 22:02:45 -0300

    configuração do multer para envio de imagens

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
@@ -6,6 +8,7 @@ import SessionController from './app/controller/SessionController';
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +16,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
