5011f452f06dbc8d0d4506cd18f630dc5fef4e6f
Mardoniosc <Mardonio@live.com>
2019-10-25 22:03:45 -0300

    Feito a migration e model de agendamento

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
@@ -16,6 +17,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
