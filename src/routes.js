23da1828133653b1c7972cf2a32a5b3c18206a82
Mardoniosc <Mardonio@live.com>
2019-10-28 12:12:28 -0300

    Criado rota e model de agendamento

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
+import AppointmentController from './app/controller/Appointment';
 import ProviderController from './app/controller/ProviderController';
 import authMiddlewares from './app/middlewares/auth';
 
@@ -19,6 +20,8 @@ routes.use(authMiddlewares);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
